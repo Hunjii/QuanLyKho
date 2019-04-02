414b8dde89a8aa5275515a34cdd8fcb7b1de3e79
mrmrkay <manhtran2198@gmail.com>
2019-04-02 21:56:56 +0700

    Thong ke

--- BackGroundFood/UserControls/UC_Issue.Designer.cs
+++ BackGroundFood/UserControls/UC_Issue.Designer.cs
@@ -28,90 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_Issue));
-            this.palUCMenu1 = new System.Windows.Forms.Panel();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.palUCMenu1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewGood = new System.Windows.Forms.ListView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonNhap = new System.Windows.Forms.Button();
+            this.buttonXuat = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // palUCMenu1
+            // label1
             // 
-            this.palUCMenu1.Controls.Add(this.btnEdit);
-            this.palUCMenu1.Controls.Add(this.btnDelete);
-            this.palUCMenu1.Controls.Add(this.btnAdd);
-            this.palUCMenu1.Location = new System.Drawing.Point(16, 3);
-            this.palUCMenu1.Name = "palUCMenu1";
-            this.palUCMenu1.Size = new System.Drawing.Size(406, 56);
-            this.palUCMenu1.TabIndex = 4;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(561, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thống kê";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // btnEdit
+            // listViewGood
             // 
-            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEdit.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
-            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEdit.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.ForeColor = System.Drawing.Color.White;
-            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
-            this.btnEdit.Location = new System.Drawing.Point(270, 0);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(125, 56);
-            this.btnEdit.TabIndex = 0;
-            this.btnEdit.Text = "Edit";
-            this.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnEdit.UseVisualStyleBackColor = true;
+            this.listViewGood.Location = new System.Drawing.Point(729, 103);
+            this.listViewGood.Name = "listViewGood";
+            this.listViewGood.Size = new System.Drawing.Size(719, 504);
+            this.listViewGood.TabIndex = 1;
+            this.listViewGood.UseCompatibleStateImageBehavior = false;
+            this.listViewGood.View = System.Windows.Forms.View.Details;
             // 
-            // btnDelete
+            // dateTimePicker1
             // 
-            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
-            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
-            this.btnDelete.Location = new System.Drawing.Point(137, 0);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(125, 56);
-            this.btnDelete.TabIndex = 0;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.dateTimePicker1.Location = new System.Drawing.Point(344, 103);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(273, 22);
+            this.dateTimePicker1.TabIndex = 2;
             // 
-            // btnAdd
+            // label2
             // 
-            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.SystemColors.Desktop;
-            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
-            this.btnAdd.Location = new System.Drawing.Point(5, 0);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(125, 56);
-            this.btnAdd.TabIndex = 0;
-            this.btnAdd.Text = "AddNew";
-            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.label2.Location = new System.Drawing.Point(201, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chọn ngày";
+            // 
+            // buttonNhap
+            // 
+            this.buttonNhap.Location = new System.Drawing.Point(406, 198);
+            this.buttonNhap.Name = "buttonNhap";
+            this.buttonNhap.Size = new System.Drawing.Size(130, 54);
+            this.buttonNhap.TabIndex = 4;
+            this.buttonNhap.Text = "Nhập";
+            this.buttonNhap.UseVisualStyleBackColor = true;
+            this.buttonNhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonXuat
+            // 
+            this.buttonXuat.Location = new System.Drawing.Point(406, 304);
+            this.buttonXuat.Name = "buttonXuat";
+            this.buttonXuat.Size = new System.Drawing.Size(130, 54);
+            this.buttonXuat.TabIndex = 5;
+            this.buttonXuat.Text = "Xuất";
+            this.buttonXuat.UseVisualStyleBackColor = true;
+            this.buttonXuat.Click += new System.EventHandler(this.buttonXuat_Click);
             // 
             // UC_Issue
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
-            this.Controls.Add(this.palUCMenu1);
+            this.Controls.Add(this.buttonXuat);
+            this.Controls.Add(this.buttonNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listViewGood);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UC_Issue";
-            this.Size = new System.Drawing.Size(1200, 600);
-            this.palUCMenu1.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(1600, 738);
+            this.Load += new System.EventHandler(this.UC_Issue_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel palUCMenu1;
-        private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewGood;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonNhap;
+        private System.Windows.Forms.Button buttonXuat;
     }
 }
--- BackGroundFood/UserControls/UC_Issue.cs
+++ BackGroundFood/UserControls/UC_Issue.cs
@@ -7,14 +7,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLKho.Controller;
 
 namespace QLKho.UserControls
 {
     public partial class UC_Issue : UserControl
     {
+        List<Goods> listGoods = new List<Goods>();
         public UC_Issue()
         {
             InitializeComponent();
+        }
+
+        private void UC_Issue_Load(object sender, EventArgs e)
+        {
+            LoadListViewGoods();
+        }
+
+        public void LoadListViewGoods()
+        {
+            listViewGood.Columns.Clear();
+            listViewGood.Items.Clear();
+            listViewGood.Columns.Add("ID").Width = 80;
+            listViewGood.Columns.Add("Name").Width = 100;
+            listViewGood.Columns.Add("Gia").Width = 100;
+            listViewGood.Columns.Add("So luong").Width = 100;
+
+            string query = string.Format("Select HANGHOA.MAHH, TENHH, DONGIA, SLCUOI from HANGHOA, TONKHO where HANGHOA.MAHH = TONKHO.MAHH");
+            DataTable result = SqlConnect.Instance.ExecuteQuery(query);
+            foreach (DataRow row in result.Rows)
+            {
+                ListViewItem item = new ListViewItem(row["MAHH"].ToString().Trim());
+                item.SubItems.Add(row["TENHH"].ToString().Trim());
+                item.SubItems.Add(row["DONGIA"].ToString().Trim());
+                item.SubItems.Add(row["SLCUOI"].ToString().Trim());
+
+                listViewGood.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime date = dateTimePicker1.Value;
+            listViewGood.Columns.Clear();
+            listViewGood.Items.Clear();
+
+            listViewGood.Columns.Add("ID").Width = 80;
+            listViewGood.Columns.Add("Name").Width = 100;
+            listViewGood.Columns.Add("So luong nhap").Width = 150;
+
+            string queryNhap = string.Format("Select NGAYNHAP, HANGHOA.MAHH, TENHH, SOLUONGNHAP" +
+                "from HANGHOA, PHIEUNHAP, CTPHIEUNHAP" +
+                "where HANGHOA.MAHH = CTPHIEUNHAP.MAHH " +
+                "and CTPHIEUNHAP.SOPN = PHIEUNHAP.SOPN" +
+                "and NGAYNHAP = {0}", date);
+            DataTable result = SqlConnect.Instance.ExecuteQuery(queryNhap);
+            foreach (DataRow row in result.Rows)
+            {
+                ListViewItem item = new ListViewItem(row["MAHH"].ToString().Trim());
+                item.SubItems.Add(row["TENHH"].ToString().Trim());
+                item.SubItems.Add(row["SOLUONGNHAP"].ToString().Trim());
+
+                listViewGood.Items.Add(item);
+            }
+        }
+
+        private void buttonXuat_Click(object sender, EventArgs e)
+        {
+            DateTime date = dateTimePicker1.Value;
+            listViewGood.Columns.Clear();
+            listViewGood.Items.Clear();
+
+            listViewGood.Columns.Add("ID").Width = 80;
+            listViewGood.Columns.Add("Name").Width = 100;
+            listViewGood.Columns.Add("So luong xuat").Width = 150;
+
+            string queryXUAT = string.Format("Select NGAYXUAT, HANGHOA.MAHH, TENHH, SLXUAT" +
+                "from HANGHOA, PHIEUXUAT, CTPHIEUXUAT" +
+                "where HANGHOA.MAHH = CTPHIEUXUAT.MAHH " +
+                "and CTPHIEUXUAT.SOPN = PHIEUXUAT.SOPN" +
+                "and NGAYXUAT = {0}", date);
+            DataTable result = SqlConnect.Instance.ExecuteQuery(queryXUAT);
+            foreach (DataRow row in result.Rows)
+            {
+                ListViewItem item = new ListViewItem(row["MAHH"].ToString().Trim());
+                item.SubItems.Add(row["TENHH"].ToString().Trim());
+                item.SubItems.Add(row["SLXUAT"].ToString().Trim());
+
+                listViewGood.Items.Add(item);
+            }
         }
     }
 }
